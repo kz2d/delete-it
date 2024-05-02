7304ddfca286a0a448e36c5842e1b88ac0128385
kz2d <avidex111@gmail.com>
2024-05-02 12:54:29 +0300

    lab1

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
 
   const configService = app.get(ConfigService);
 
   const port = configService.get('PORT');
-  console.log(port)
-  console.log(process.env.PORT)
 
   await app.listen(port);
 }
